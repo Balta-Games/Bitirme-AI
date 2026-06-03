aae32a892f10f0aa2f9368c6141424d8a2257867
Güneş Balcı <gunesbalci66@hotmail.com>
2026-06-04 00:17:13 +0300

    Models can be transformed into onnx file to use with unity.

--- train_XGBoost.py
+++ train_XGBoost.py
@@ -2,26 +2,50 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 import pandas as pd
+import joblib
+import onnxmltools
+from onnxmltools.convert.common.data_types import FloatTensorType
 
-df = pd.read_csv('Dataset/soulslike_500.csv')
-x = df.drop(columns=['is_won']) 
-y = df['is_won']
+def train_xgboost(file_path):
+    df = pd.read_csv(file_path)
+    x = df.drop(columns=['is_won']) 
+    y = df['is_won']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-model = xgb.XGBClassifier(
-    n_estimators=100,      # tree number
-    learning_rate=0.1,     
-    max_depth=3,           # tree depth
-    random_state=42
-)
+    model = xgb.XGBClassifier(
+        n_estimators=100,      # tree number
+        learning_rate=0.1,     
+        max_depth=3,           # tree depth
+        random_state=42
+    )
 
-model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
+    model.fit(x_train.values, y_train.values)
+    y_pred = model.predict(x_test.values)
 
-model.save_model('Models/XGBoost/model.json')
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average='macro')
+    print(f"Modelin Doğruluk Skoru (Accuracy): {accuracy * 100:.2f}%")
+    print(f"Modelin F1 Skoru: {f1:.2f}")
 
-accuracy = accuracy_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred, average='macro')
-print(f"Modelin Doğruluk Skoru (Accuracy): {accuracy * 100:.2f}%")
-print(f"Modelin F1 Skoru: {f1:.2f}")
+    return model
+
+def save_model(model):
+    model.save_model('Models/XGBoost/model.json')
+
+def transform_model2ONNX():
+    xgb_model = xgb.XGBClassifier()
+    xgb_model.load_model('Models/XGBoost/model.json')
+
+    #Veriniz kaç sütundan oluşuyorsa (Örn: 4 sütun) buraya yazın
+    initial_type = [('float_input', FloatTensorType([None, 5]))]
+
+    onnx_xgb = onnxmltools.convert_xgboost(xgb_model, initial_types=initial_type)
+    with open("Models/XGBoost/model.onnx", "wb") as f:
+        f.write(onnx_xgb.SerializeToString())
+
+#-------------------------------------------------------------------------
+
+model = train_xgboost('Dataset/soulslike_500.csv')
+save_model(model)
+transform_model2ONNX()
--- train_kmeans.py
+++ train_kmeans.py
@@ -1,15 +1,41 @@
 import pandas as pd
 from sklearn.cluster import KMeans
-import joblib
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+import joblib
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
 
-df = pd.read_csv('Dataset/soulslike_500.csv')
 
-scaler = StandardScaler()
-df_scaled = scaler.fit_transform(df)
+def train_kmeans(file_path):
+    df = pd.read_csv(file_path)
 
-kmeans = KMeans(n_clusters=2, random_state=42)
-kmeans.fit(df_scaled)
+    scaler = StandardScaler()
+    df_scaled = scaler.fit_transform(df)
 
-joblib.dump(kmeans, 'Models/Kmeans/model.joblib')
-joblib.dump(scaler, 'Models/Kmeans/scaler.joblib')
+    kmeans = KMeans(n_clusters=2, random_state=42)
+    kmeans.fit(df_scaled)
+    return kmeans, scaler
+
+def save_model(kmeans, scaler):
+    joblib.dump(kmeans, 'Models/Kmeans/model.joblib')
+    joblib.dump(scaler, 'Models/Kmeans/scaler.joblib')
+
+def transform_model2ONNX():
+    kmeans_model = joblib.load('Models/Kmeans/model.joblib')
+    kmeans_scaler = joblib.load('Models/Kmeans/scaler.joblib')
+
+    #Veriniz kaç sütundan oluşuyorsa (Örn: 4 sütun) buraya yazın
+    initial_type = [('float_input', FloatTensorType([None, 5]))] 
+
+    pipeline = Pipeline([('scaler', kmeans_scaler), ('kmeans', kmeans_model)])
+    onnx_kmeans = convert_sklearn(pipeline, initial_types=initial_type)
+
+    with open("Models/Kmeans/pipeline.onnx", "wb") as f:
+        f.write(onnx_kmeans.SerializeToString())
+
+#-------------------------------------------------------------------------------
+
+#kmeans_model, scaler = train_kmeans('Dataset/soulslike_500.csv')
+#save_model(kmeans_model, scaler)
+transform_model2ONNX()
